0a69470a014aebefe086d6c86829821bd34efc51
Paul Fox <paul@homemadedigital.com>
2013-03-09 17:37:33 +0000

    Timing and memory use

--- test/lexer.php
+++ test/lexer.php
@@ -5,13 +5,15 @@ use AmperXIL\Source\XILFileSource;
 
 require_once "autoload.php";
 
+defined('TIME_START') || define('TIME_START',microtime());
+
 try
 {
 	$parser = new Parser( XILFileSource::find("prototype.xil") );
 
 	$parser->parse();
 
-	echo 'OK';
+	printf("OK. Peak memory used: %fMB. Execution time: %fs\n", memory_get_peak_usage(true) / 1024 / 1024, round((microtime() - TIME_START), 4));
 }
 catch (AmperXIL\Parser\ParserException $e)
 {
